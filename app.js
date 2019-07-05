3aabff8484ddaaa75a91e495a2d1a2ec83d2b737
dheyme-jensen <dheymejensen@hotmail.com>
2019-07-05 15:36:41 -0300

    app routes

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const db = require('./models/index');
 
+app.use(express.json());
+
 app.listen(4667, console.log('servidor rodando'));
 app.use('/users', require('./routes/user'));
+app.use('/products', require('./routes/products'));
+app.use('/orders', require('./routes/orders'));
 
 db.sequelize.sync();
